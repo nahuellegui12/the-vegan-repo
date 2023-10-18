c3149527d82e9162a1a14dd7a200687667bd32aa
nahuellegui12 <nahueljesusleguizamon@gmail.com>
2023-10-17 21:13:29 -0300

    fix: crea estructura de heade

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import '../Banner/BannerStyle.css'
+
+const Banner = () => {
+  return (
+    <div className='banner-container'>
+        <div className='banner-content'>
+            <div className='animal-image-content'>
+
+            </div>
+            <div className='info-banner-content'>
+                <h1 className='banner-title'>Go Vegan</h1>
+                <p className='banner-subtitle'>Lorem ipsum dolor sit amet</p>
+                <h2 className='banner-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sequi excepturi reiciendis inventore asperiores error nulla, reprehenderit, quae mollitia atque blanditiis minima adipisci quam quis vero sapiente dolores! Cum, laboriosam.</h2>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import '../Header/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div className='header'>
+        <p className='header-title'>Hola</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
